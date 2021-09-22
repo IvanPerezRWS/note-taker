cf9f78a2fa820e42e6bb79d1eda70ed681890c88
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-09-21 20:31:54 -0500

    setup localhost funcitonality

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ const express = require('express');
 // creating a server
 const app = express();
 
+//Setting PORT listener
+const PORT = process.nextTick.PORT || 3000;
+
+
+// api call response
+app.get("/api/notes", function (err, res) {
+    try {
+      createNoteData = fs.readFileSync("db/db.json", "utf8");
+      console.log("Hello from the SERVER!");
+      createNoteData = JSON.parse(createNoteData);
+    } catch (err) {
+      console.log("\n error (catch err app.get):");
+      console.log(err);
+    }
+    res.json(createNoteData);
+  });
+
 // Start server on port
-app.listen(3001, () => {
-    console.log("Server is online:" + PORT);
-})
+app.listen(PORT, function () {
+    console.log("SERVER IS ONLINE: " + PORT);
+  });
