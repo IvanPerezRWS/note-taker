6b1817a600a8007c8c86534cd405b4617881f0b7
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-09-23 20:29:15 -0500

    Set up createNoteArray, added api call response, write notes to file, POST and GET functionality.

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const app = express();
 //Setting PORT listener
 const PORT = process.nextTick.PORT || 3000;
 
+// create noteData Array
+let createNoteData = [];
+
+// middleware parsing, stati and route
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // api call response
 app.get("/api/notes", function (err, res) {
@@ -21,6 +33,54 @@ app.get("/api/notes", function (err, res) {
     }
     res.json(createNoteData);
   });
+
+// writes new note to json file for browser
+app.post("/api/notes", function (req, res) {
+    try {
+        createNoteData = fs.readFileSync("./db/db.json", utf8);
+        console.log(createNoteData);
+        createNoteData = JSON.parse(createNoteData);
+        req.body.id = createNoteData.length;
+        createNoteData.push(req.body);
+        createNoteData = JSON.stringify(createNoteData);
+        fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) {
+            if (err) throw err;
+        });
+
+        res.json(JSON.parse(createNoteData));
+    } catch (err) {
+        throw err;
+        console.error(err);
+    }
+});
+
+// Delete note, read json file. Write new notes to browser.
+app.delete("/api/notes/:id", function (req, res) {
+    try {
+        createNoteData = fs.readFileSync("./db/db.json", "utf8");
+        createNoteData = JSON.parse(createNoteData);
+        createNoteData = createNoteData.filter(function (note) {
+            return note.id != req.params.id;
+        });
+        createNoteData = JSON.stringify(createNoteData);
+
+        fs.writeFile("./db/db.sjon", createNoteData, "utf8", function (err) {
+            if (err) {
+                throw err;
+                console.log(err);
+            }
+        });
+    }
+});
+
+
+
+
+
+// Default to home if no route is found
+app.get("*", function (req, res) {
+    return res.sendFile(path.json(__dirname, :db/db.json));
+});
 
 // Start server on port
 app.listen(PORT, function () {
